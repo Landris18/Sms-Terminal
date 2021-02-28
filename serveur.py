755f0aeac17b6335d28c2f24680c4794241796e6
Landry Manankoraisina <47665507+Landris18@users.noreply.github.com>
2021-02-28 15:24:43 +0300

    Update serveur.py

--- serveur.py
+++ serveur.py
@@ -8,9 +8,9 @@ try:
 	connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	connexion_principale.bind((hote, port))
 	
-	msg_a_envoyer = b"salut"
+	msg_a_envoyer = b"Starting..."
 	
-	while msg_a_envoyer != b"fin":
+	while msg_a_envoyer != b"Aborting...":
 		msg_a_envoyer = input(">>> ")
 		msg_a_envoyer.encode()
 		connexion_avec_server.send(msg_a_envoyer)
@@ -21,4 +21,4 @@ try:
 	connexion_avec_server.close()
 
 except:
-	print("svp, Lancer le serveur")
+	print("Lancez le serveur")
